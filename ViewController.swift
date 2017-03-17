4f0247b24bd48edd1c0a6ca77f226178877674f9
Soni, Ankita (US - Bengaluru) <anksoni@deloitte.com>
2017-03-17 12:08:54 +0530

    First commit

---
+++ ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  DemoProject
+//
+//  Created by Soni, Ankita (US - Bengaluru) on 3/17/17.
+//  Copyright © 2017 Soni, Ankita (US - Bengaluru). All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
